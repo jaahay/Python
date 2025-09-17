9897640e68bd4371e062b2baa9321516b4c813a4
James <blutegel@users.noreply.github.com>
2025-09-17 16:34:19 -0700

    commit 4 changed main file

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ from Guess import guess_game
 #from RockPaperScissors_File import rockpaperscissors_game
 
 def main():
-    User_selection = int(input("Enter your choice: \n"
+    user_selection = int(input("Enter your choice: \n"
                                "1. Random Number Guessing Game\n"
                                "2. Rock Paper Scissors Game\n"
                                "3. Exit\n"))
-    while User_selection != 3:
-        if User_selection == 1:
+    while user_selection != 3:
+        if user_selection == 1:
             guess_game()
-        elif User_selection == 2:
+        elif user_selection == 2:
             #rockpaperscissors_game()
             print("You chose an invalid option.")
-        elif User_selection == 3:
+        elif user_selection == 3:
             print("Thank you for playing!")
             break
 if __name__ == "__main__":
